a9d6a8be3a7f0504556ebc9836e1095541de1577
Ria Mundhra <rm2060@cam.ac.uk>
2025-02-10 14:50:15 +0000

    landing page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import SignInScreen from './sign-in-side/SignInSide'
+import LandingPage from "./marketing-page/MarketingPage"
 
 function App() {
   return (
-   <SignInScreen />
+    <BrowserRouter>
+    <NavigationHeader />
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/signin" element={<SignInScreen />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
