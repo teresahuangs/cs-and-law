c4130a75e88d0143f3716c707310af625308b547
Ria Mundhra <rm2060@cam.ac.uk>
2025-02-10 14:38:08 +0000

    ui boilerplate

---
+++ ui/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import SignInScreen from './sign-in-side/SignInSide'
+
+function App() {
+  return (
+   <SignInScreen />
+  );
+}
+
+export default App;
